15fffcb9940844b0033fb5993ef4a76db52157ab
Kuwerin <45897893+Kuwerin@users.noreply.github.com>
2022-01-22 13:05:30 +0300

    Update main function

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,10 @@
+from typing import NoReturn, Optional
 import uvicorn
 
 from transport import app
 
-uvicorn.run(app, host="0.0.0.0", port=5000, debug=True)
+def main() -> Optional[NoReturn]:
+    uvicorn.run(app, host="0.0.0.0", port=5000, debug=True, timeout_keep_alive=0)
+
+main()
 
